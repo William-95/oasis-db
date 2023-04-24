4b94fb1058c51df15e8da4ae762a8cecd46c3ee1
William-95 <verga.william.95@gmail.com>
2023-04-24 17:06:16 +0200

    s

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -8,14 +8,15 @@ use App\Models\Dog;
 
 class DogController extends Controller
 {
-       // readDog
+  // readDog
   public function readDog()
   {
     $dogs = DB::table("dog")
       ->select("*")
       ->get();
-
+      
     return response()->json($dogs);
+
   }
 
   // createDog
@@ -23,60 +24,44 @@ class DogController extends Controller
   {
     $dogs = new Dog();
 
-    $cleaned_name = filter_var(
-      $request->input("name"),
+    $cleaned_name = filter_var($request->input("name"), FILTER_SANITIZE_STRING);
+
+    $cleaned_sex = filter_var($request->input("sex"), FILTER_SANITIZE_STRING);
+
+    $cleaned_race = filter_var($request->input("race"), FILTER_SANITIZE_STRING);
+
+    $cleaned_size = filter_var($request->input("size"), FILTER_SANITIZE_STRING);
+
+    $cleaned_date_birth = filter_var(
+      $request->input("date_birth"),
       FILTER_SANITIZE_STRING
     );
 
-    $cleaned_sex = filter_var(
-      $request->input("sex"),
+    $cleaned_microchip = filter_var(
+      $request->input("microchip"),
+      FILTER_SANITIZE_NUMBER_INT
+    );
+
+    $cleaned_date_entry = filter_var(
+      $request->input("date_entry"),
       FILTER_SANITIZE_STRING
     );
 
-    $cleaned_race = filter_var(
-        $request->input("race"),
-        FILTER_SANITIZE_STRING
-      );
+    $cleaned_structure = filter_var(
+      $request->input("structure"),
+      FILTER_SANITIZE_STRING
+    );
 
-      $cleaned_size = filter_var(
-        $request->input("size"),
-        FILTER_SANITIZE_STRING
-      );
+    $cleaned_contacts = filter_var(
+      $request->input("contacts"),
+      FILTER_SANITIZE_STRING
+    );
 
-      $cleaned_date_birth = filter_var(
-        $request->input("date_birth"),
-        FILTER_SANITIZE_STRING
-      );
+    if ($request->hasFile('img')) {
+      $path= $request->file('img')->move(public_path('public/images'), $request->file('img')->getClientOriginalName());
+      $dogs->img=$path;
 
-      $cleaned_microchip = filter_var(
-        $request->input("microchip"),
-        FILTER_SANITIZE_NUMBER_INT
-      );
-
-      $cleaned_date_entry = filter_var(
-        $request->input("date_entry"),
-        FILTER_SANITIZE_STRING
-      );
-
-      $cleaned_structure = filter_var(
-        $request->input("structure"),
-        FILTER_SANITIZE_STRING
-      );
-
-      $cleaned_contacts = filter_var(
-        $request->input("contacts"),
-        FILTER_SANITIZE_STRING
-      );
-
-      // $cleaned_img = filter_var(
-      //   $request->file("img")->store('storage'),
-      //   FILTER_SANITIZE_URL
-      // );
-      if ($request->hasFile('img')) {
-        $path= $request->file('img')->move('public/images', $request->file('img')->getClientOriginalName());
-        $dogs->img=$path;
-
-      }
+    }
 
     $dogs->name = $cleaned_name;
     $dogs->sex = $cleaned_sex;
@@ -85,77 +70,53 @@ class DogController extends Controller
     $dogs->date_birth = $cleaned_date_birth;
     $dogs->microchip = $cleaned_microchip;
     $dogs->date_entry = $cleaned_date_entry;
-    
-  //  $dogs->img = $request->file('img')->move('public/images', $request->file('img')->getClientOriginalName());
-
     $dogs->structure = $cleaned_structure;
     $dogs->contacts = $cleaned_contacts;
 
-    
-
-    
     $dogs->save();
 
     return response()->json($dogs);
-    
-
   }
 
-
   // updateDog
-  public function updateDog(Request $request,$id)
+  public function updateDog(Request $request, $id)
   {
-    $dogs =Dog::find($id);
+    $dogs = Dog::find($id);
 
-    $cleaned_name = filter_var(
-      $request->input("name"),
+    $cleaned_name = filter_var($request->input("name"), FILTER_SANITIZE_STRING);
+
+    $cleaned_sex = filter_var($request->input("sex"), FILTER_SANITIZE_STRING);
+
+    $cleaned_race = filter_var($request->input("race"), FILTER_SANITIZE_STRING);
+
+    $cleaned_size = filter_var($request->input("size"), FILTER_SANITIZE_STRING);
+
+    $cleaned_date_birth = filter_var(
+      $request->input("date_birth"),
       FILTER_SANITIZE_STRING
     );
 
-    $cleaned_sex = filter_var(
-      $request->input("sex"),
+    $cleaned_microchip = filter_var(
+      $request->input("microchip"),
+      FILTER_SANITIZE_NUMBER_INT
+    );
+
+    $cleaned_date_entry = filter_var(
+      $request->input("date_entry"),
       FILTER_SANITIZE_STRING
     );
 
-    $cleaned_race = filter_var(
-        $request->input("race"),
-        FILTER_SANITIZE_STRING
-      );
+    $cleaned_img = filter_var($request->input("img"), FILTER_SANITIZE_URL);
 
-      $cleaned_size = filter_var(
-        $request->input("size"),
-        FILTER_SANITIZE_STRING
-      );
+    $cleaned_structure = filter_var(
+      $request->input("structure"),
+      FILTER_SANITIZE_STRING
+    );
 
-      $cleaned_date_birth = filter_var(
-        $request->input("date_birth"),
-        FILTER_SANITIZE_STRING
-      );
-
-      $cleaned_microchip = filter_var(
-        $request->input("microchip"),
-        FILTER_SANITIZE_NUMBER_INT
-      );
-
-      $cleaned_date_entry = filter_var(
-        $request->input("date_entry"),
-        FILTER_SANITIZE_STRING
-      );
-
-      $cleaned_img = filter_var(
-        $request->input("img"),
-        FILTER_SANITIZE_URL
-      );
-
-      $cleaned_structure = filter_var(
-        $request->input("structure"),
-        FILTER_SANITIZE_STRING
-      );
-
-      $cleaned_contacts = filter_var(
-        $request->input("contacts"),
-        FILTER_SANITIZE_STRING
-      );
+    $cleaned_contacts = filter_var(
+      $request->input("contacts"),
+      FILTER_SANITIZE_STRING
+    );
 
     $dogs->name = $cleaned_name;
     $dogs->sex = $cleaned_sex;
@@ -168,37 +129,34 @@ class DogController extends Controller
     $dogs->structure = $cleaned_structure;
     $dogs->contacts = $cleaned_contacts;
 
-
-    
     $dogs->save();
-        
 
     return response()->json($dogs);
-   
   }
 
   //   deleteDog
-public function deleteDog($id)
-{
-  $cleaned_id = filter_var($id,FILTER_SANITIZE_STRING);
-  DB::delete('delete from dog where id=?',[$cleaned_id]);
+  public function deleteDog($id)
+  {
+    $cleaned_id = filter_var($id, FILTER_SANITIZE_STRING);
+    DB::delete("delete from dog where id=?", [$cleaned_id]);
 
-  return response()->json("Cane cancellato.");
-}
+    return response()->json("Cane cancellato.");
+  }
 
-// ------------------- // ------------------- // ------------------- // ------------------- //  
-// findDog
-public function findDog(Request $request){
+  // ------------------- // ------------------- // ------------------- // ------------------- //
+  // findDog
+  public function findDog(Request $request)
+  {
     $cleaned_microchip = filter_var(
       $request->input("microchip"),
       FILTER_SANITIZE_STRING
     );
-      
-    $dog= DB::table('dog')
-   ->select('*')
-   ->where('microchip',$cleaned_microchip)
-   ->get();
-  
-   return response()->json($dog);
+
+    $dog = DB::table("dog")
+      ->select("*")
+      ->where("microchip", $cleaned_microchip)
+      ->get();
+
+    return response()->json($dog);
   }
 }
