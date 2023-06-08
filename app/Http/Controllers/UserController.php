b3688ffc83b89e1a12822c39901325061a2d2c62
William-95 <verga.william.95@gmail.com>
2023-06-08 11:59:33 +0200

    a

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,16 +18,12 @@ class UserController extends Controller
     return response()->json($user);
   }
 
-
   // createUser
   public function createUser(Request $request)
   {
     $user = new User();
 
-    $cleaned_name = filter_var(
-      $request->input("name"),
-      FILTER_SANITIZE_STRING
-    );
+    $cleaned_name = filter_var($request->input("name"), FILTER_SANITIZE_STRING);
 
     $cleaned_email = filter_var(
       $request->input("email"),
@@ -35,40 +31,35 @@ class UserController extends Controller
     );
 
     $cleaned_password = filter_var(
-        $request->input("password"),
-        FILTER_SANITIZE_STRING
-      );
+      $request->input("password"),
+      FILTER_SANITIZE_STRING
+    );
 
-      $cleaned_confirm_password = filter_var(
-        $request->input("confirm_password"),
-        FILTER_SANITIZE_STRING
-      );
+    $cleaned_confirm_password = filter_var(
+      $request->input("confirm_password"),
+      FILTER_SANITIZE_STRING
+    );
 
     $user->name = $cleaned_name;
     $user->email = $cleaned_email;
     $user->password = $cleaned_password;
     $user->confirm_password = $cleaned_confirm_password;
 
-    if($cleaned_password==$cleaned_confirm_password){
-        $user->save();
+    if ($cleaned_password == $cleaned_confirm_password) {
+      $user->save();
 
-    return response()->json($user);
-    } else{
-        return response()->json('Utente non creato.Password non confermata');
+      return response()->json($user);
+    } else {
+      return response()->json("Utente non creato.Password non confermata");
     }
-
   }
 
-
-//   updateUser
-public function updateUser(Request $request,$id)
+  //   updateUser
+  public function updateUser(Request $request, $id)
   {
     $user = User::find($id);
 
-    $cleaned_name = filter_var(
-      $request->input("name"),
-      FILTER_SANITIZE_STRING
-    );
+    $cleaned_name = filter_var($request->input("name"), FILTER_SANITIZE_STRING);
 
     $cleaned_email = filter_var(
       $request->input("email"),
@@ -76,57 +67,62 @@ public function updateUser(Request $request,$id)
     );
 
     $cleaned_password = filter_var(
-        $request->input("password"),
-        FILTER_SANITIZE_STRING
-      );
+      $request->input("password"),
+      FILTER_SANITIZE_STRING
+    );
 
-      $cleaned_confirm_password = filter_var(
-        $request->input("confirm_password"),
-        FILTER_SANITIZE_STRING
-      );
-
-    $user->name = $cleaned_name;
-    $user->email = $cleaned_email;
-    $user->password = $cleaned_password;
-    $user->confirm_password = $cleaned_confirm_password;
-
-    if($cleaned_password==$cleaned_confirm_password){
-        $user->save();
-
-    return response()->json($user);
-    } else{
-        return response()->json('Utente non modificato correttamente.Password non confermata');
+    $cleaned_confirm_password = filter_var(
+      $request->input("confirm_password"),
+      FILTER_SANITIZE_STRING
+    );
+    if (!empty($cleaned_name)) {
+      $user->name = $cleaned_name;
+    }
+    if (!empty($cleaned_email)) {
+      $user->email = $cleaned_email;
+    }
+    if (!empty($cleaned_password)) {
+      $user->password = $cleaned_password;
+    }
+    if (!empty($cleaned_confirm_password)) {
+      $user->confirm_password = $cleaned_confirm_password;
     }
 
+    if ($cleaned_password == $cleaned_confirm_password) {
+      $user->save();
+
+      return response()->json($user);
+    } else {
+      return response()->json(
+        "Utente non modificato correttamente.Password non confermata"
+      );
+    }
   }
-//   deleteUser
-public function deleteUser($id)
-{
-  $cleaned_id = filter_var($id,FILTER_SANITIZE_STRING);
-  DB::delete('delete from user where id=?',[$cleaned_id]);
+  //   deleteUser
+  public function deleteUser($id)
+  {
+    $cleaned_id = filter_var($id, FILTER_SANITIZE_STRING);
+    DB::delete("delete from user where id=?", [$cleaned_id]);
 
-  return response()->json("User delete.");
+    return response()->json("User delete.");
+  }
+
+  // ------------------- // ------------------- // ------------------- // ------------------- //
+  // findUser
+  public function findUser(Request $request)
+  {
+    $cleaned_name = filter_var($request->input("name"), FILTER_SANITIZE_STRING);
+    $cleaned_password = filter_var(
+      $request->input("password"),
+      FILTER_SANITIZE_STRING
+    );
+
+    $user = DB::table("user")
+      ->select("*")
+      ->where("name", $cleaned_name)
+      ->where("password", $cleaned_password)
+      ->get();
+
+    return response()->json($user);
+  }
 }
-
-// ------------------- // ------------------- // ------------------- // ------------------- //  
-// findUser
-public function findUser(Request $request){
-  $cleaned_name = filter_var(
-    $request->input("name"),
-    FILTER_SANITIZE_STRING
-  );
-  $cleaned_password = filter_var(
-    $request->input("password"),
-    FILTER_SANITIZE_STRING
-  );
- 
-  $user= DB::table('user')
- ->select('*')
- ->where('name',$cleaned_name)
- ->where ('password',$cleaned_password)
- ->get();
-
- return response()->json($user);
-}
-}
-
