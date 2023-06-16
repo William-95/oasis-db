4d99d35f8b321bb4eb331522bbe04f6974e7b8d0
William-95 <verga.william.95@gmail.com>
2023-06-16 17:28:22 +0200

    a

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,7 @@ return [
         // ],
         'public' => [
             'driver' => 'local',
-            'root' => public_path('public\public\images'),
+            'root' => public_path('public\images'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
