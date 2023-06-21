17bc36c72d60c2b46ae15280f72ec1b522059918
William-95 <verga.william.95@gmail.com>
2023-06-21 18:53:48 +0200

    Update 2023_04_01_145743_create_dog_table.php

--- database/migrations/2023_04_01_145743_create_dog_table.php
+++ database/migrations/2023_04_01_145743_create_dog_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('race');
             $table->string('size');
             $table->date('date_birth');
-            $table->integer('microchip')->unique();
+            $table->bigInteger('microchip')->unique();
             $table->date('date_entry');
             $table->string('img');
             $table->string('region');
