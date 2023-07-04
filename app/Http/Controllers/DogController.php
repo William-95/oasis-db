bda3564029841e687ad0952aab3595b3ffe09d01
William-95 <verga.william.95@gmail.com>
2023-07-04 19:14:09 +0200

    s

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -118,9 +118,6 @@ if ($request->hasFile("img")) {
 ]);
 }
 
-   
-
-
     // $dogs = new Dog();
 
     // // ------img
@@ -168,53 +165,52 @@ if ($request->hasFile("img")) {
     // $dogs->save();
     // return response()->json($dogs);
   }
-
+// ------------------- // ------------------- // ------------------- // ------------------- //
   // updateDog
   public function updateDog(Request $request, $id)
   {
-    $dogs = Dog::find($id);
 
-    // $cleaned_name = filter_var($request->input("name"), FILTER_SANITIZE_STRING);
+    $validator = Validator::make($request->all(), [
+      "name" => "required|string|max:100",
+      "sex" => "required|string|max:100",
+      "race" => "required|string|max:100",
+      "size" => "required|string|max:100",
+      "date_birth" => "required|date",
+      "microchip" => "required|numeric|regex:/^\d+$/|unique:dog",
+      "date_entry" => "required|date",
+      "img" => "required|image",
+      "region" => "required|string|max:100",
+      "structure" => "required|string|max:100",
+      "contacts" => "required|string|max:100",
+    ]);
+   
+    $dog = Dog::find($id);
 
-    // $cleaned_sex = filter_var($request->input("sex"), FILTER_SANITIZE_STRING);
+    if ($dog->microchip !== $request->input("microchip")) {
+      if (Dog::where("microchip", $request->microchip)->exists()) {
+        return response()->json(
+          [
+            "success" => false,
+            "message" => "Microchip esistente.",
+          ],
+          400
+        );
+      }
+    }
+    if ($validator->fails()) {
+      return response()->json(
+        [
+          "message" => "validation fails",
+          "errors" => $validator->errors(),
+        ],
+        400
+      );
+    }
 
-    // $cleaned_race = filter_var($request->input("race"), FILTER_SANITIZE_STRING);
-
-    // $cleaned_size = filter_var($request->input("size"), FILTER_SANITIZE_STRING);
-
-    // $cleaned_date_birth = filter_var(
-    //   $request->input("date_birth"),
-    //   FILTER_SANITIZE_STRING
-    // );
-
-    // $cleaned_microchip = filter_var(
-    //   $request->input("microchip"),
-    //   FILTER_SANITIZE_NUMBER_INT
-    // );
-
-    // $cleaned_date_entry = filter_var(
-    //   $request->input("date_entry"),
-    //   FILTER_SANITIZE_STRING
-    // );
-
-    // $cleaned_region = filter_var(
-    //   $request->input("region"),
-    //   FILTER_SANITIZE_STRING
-    // );
-
-    // $cleaned_structure = filter_var(
-    //   $request->input("structure"),
-    //   FILTER_SANITIZE_STRING
-    // );
-
-    // $cleaned_contacts = filter_var(
-    //   $request->input("contacts"),
-    //   FILTER_SANITIZE_STRING
-    // );
     // ---------------img
     if (!empty($request->file("img"))) {
       if ($request->hasFile("img")) {
-        $destination = $dogs->img;
+        $destination = $dog->img;
         $image = $request->file("img");
         $fileName = time() . "-" . $image->getClientOriginalName();
         $ApiKey = env("API_KEY");
@@ -234,7 +230,7 @@ if ($request->hasFile("img")) {
 
         if ($response->successful()) {
           $imageUrl = $response->json("data.url");
-          $dogs->img = $imageUrl;
+          $dog->img = $imageUrl;
         } else {
           $imageError = $response->json("error.message");
           return response()->json([
@@ -242,46 +238,59 @@ if ($request->hasFile("img")) {
           ]);
         }
       } else {
-        return "no file";
+        return response()->json([
+          "success" => false,
+          "error" => "Nessun file immagine fornito.",
+      ]);
       }
     }
     //------------ end img
     if (!empty($request->input("name"))) {
-      $dogs->name = ucfirst($request->input("name"));
+      $dog->name = ucfirst($request->input("name"));
     }
     if (!empty($request->input("sex"))) {
-      $dogs->sex = ucfirst($request->input("sex"));
+      $dog->sex = ucfirst($request->input("sex"));
     }
     if (!empty($request->input("race"))) {
-      $dogs->race = ucwords($request->input("race"));
+      $dog->race = ucwords($request->input("race"));
     }
     if (!empty($request->input("size"))) {
-      $dogs->size = ucfirst($request->input("size"));
+      $dog->size = ucfirst($request->input("size"));
     }
     if (!empty($request->input("date_birth"))) {
-      $dogs->date_birth = $request->input("date_birth");
+      $dog->date_birth = $request->input("date_birth");
     }
     if (!empty($request->input("microchip"))) {
-      $dogs->microchip = $request->input("microchip");
+      $dog->microchip = $request->input("microchip");
     }
     if (!empty($request->input("date_entry"))) {
-      $dogs->date_entry = $request->input("date_entry");
+      $dog->date_entry = $request->input("date_entry");
     }
     if (!empty($request->input("region"))) {
-      $dogs->region = ucwords($request->input("region"));
+      $dog->region = ucwords($request->input("region"));
     }
     if (!empty($request->input("structure"))) {
-      $dogs->structure = ucwords($request->input("structure"));
+      $dog->structure = ucwords($request->input("structure"));
     }
     if (!empty($request->input("contacts"))) {
-      $dogs->contacts = $request->input("contacts");
+      $dog->contacts = $request->input("contacts");
     }
 
-    $dogs->save();
+    $dog->save();
 
-    return response()->json($dogs);
+    $data = [
+      [
+        "metadata" => [
+          "success" => true,
+          "message" => "Cane modificato con successo.",
+        ],
+        "data" => $dog,
+      ],
+    ];
+
+    return response()->json($data);
   }
-
+// ------------------- // ------------------- // ------------------- // ------------------- //
   //   deleteDog
   public function deleteDog($id)
   {
@@ -299,15 +308,7 @@ if ($request->hasFile("img")) {
   // findDog
   public function findDog(Request $request)
   {
-    // $cleaned_microchip = filter_var(
-    //   $request->input("microchip"),
-    //   FILTER_SANITIZE_STRING
-    // );
-
-    // $dog = DB::table("dog")
-    //   ->select("*")
-    //   ->where("microchip", $cleaned_microchip)
-    //   ->get();
+   
     $microchip = $request->input("microchip");
 
     $dog = Dog::where("microchip", $microchip)->get();
